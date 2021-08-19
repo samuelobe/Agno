d8487049bfc28fc8f02e5dd3086fcd1358b1efc5
Samuel Obe <samuel.obe@outlook.com>
2021-08-19 17:28:37 -0400

    Added detail view to app

--- Agno/View/CameraView.swift
+++ Agno/View/CameraView.swift
@@ -11,50 +11,58 @@ import AVFoundation
 struct CameraView : View {
     @StateObject var camera = CameraViewModel()
     var isPreview = Platform.isSimulator
+    @State private var action: Int? = 0
     
     
     var body: some View {
-        ZStack {
-            if isPreview {
-                Color.gray
-                Text("Camera View").foregroundColor(.white)
-            }
-            else{
-                CameraPreview(camera: camera)
-            }
-            VStack{
-                Spacer()
+        NavigationView {
+            NavigationLink(destination: DetailView(), tag: 1, selection: $action) {
                 ZStack {
-                    Color.black.frame(height: 85, alignment: .center).opacity(0.75)
-                    HStack{
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            Image(systemName: "photo").font(.system(size: 27.0)).foregroundColor(.white)
-                        }).padding(.leading, 40)
-                        Spacer()
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            Image(systemName: "gearshape").font(.system(size: 27.0)).foregroundColor(.white)
-                        }).padding(.trailing, 40)
-                    }
-                }
-            }
-            VStack {
-                Spacer()
-                HStack{
-                    ZStack {
-                        Button(action: {
-                            camera.isPhotoTaken.toggle()
-                            print(camera.isPhotoTaken)
-                        }, label: {
-                            Circle().fill(Color("ButtonColor")).frame(width: 70, height: 70, alignment: .center)
-                        })
-                        Circle().stroke(Color.white, lineWidth: 5).frame(width: 85, height: 85, alignment: .center)
-                    }
                     
-                }.padding(.bottom, 40)
+                    if isPreview {
+                        Color.gray
+                        Text("Camera View").foregroundColor(.white)
+                    }
+                    else{
+                        CameraPreview(camera: camera)
+                    }
+                    VStack{
+                        Spacer()
+                        ZStack {
+                            Color.black.frame(height: 85, alignment: .center).opacity(0.75)
+                            HStack{
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    Image(systemName: "photo").font(.system(size: 27.0)).foregroundColor(.white)
+                                }).padding(.leading, 40)
+                                Spacer()
+                                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                                    Image(systemName: "gearshape").font(.system(size: 27.0)).foregroundColor(.white)
+                                }).padding(.trailing, 40)
+                            }
+                        }
+                    }
+                    VStack {
+                        Spacer()
+                        HStack{
+                            ZStack {
+                                Button(action: {
+                                    camera.isPhotoTaken.toggle()
+                                    self.action = 1
+                                }, label: {
+                                    Circle().fill(Color("ButtonColor")).frame(width: 70, height: 70, alignment: .center)
+                                })
+                                Circle().stroke(Color.white, lineWidth: 5).frame(width: 85, height: 85, alignment: .center)
+                            }
+                            
+                        }.padding(.bottom, 40)
+                    }
+                }.onAppear(perform: {
+                    camera.check()
+                }).ignoresSafeArea(.all ,edges: .all)
             }
-        }.onAppear(perform: {
-            camera.check()
-        }).ignoresSafeArea(.all ,edges: .all)
+            
+        }
+        
     }
 }
 
@@ -62,7 +70,7 @@ struct Platform {
     static let isSimulator: Bool = {
         var isSim = false
         #if targetEnvironment(simulator)
-          isSim = true
+        isSim = true
         #endif
         return isSim
     }()
---
+++ Agno/View/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Agno
+//
+//  Created by Sam Obe on 8/19/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
