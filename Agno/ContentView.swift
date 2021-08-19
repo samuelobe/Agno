cfbef247db918dd8b999793b4ffca986ff42d70d
Samuel Obe <samuel.obe@outlook.com>
2021-08-19 10:04:42 -0400

    Initial Commit

---
+++ Agno/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Agno
+//
+//  Created by Sam Obe on 8/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
