1f7a94be2cd4c1485aface2e996b60c4ff04274a
Samuel Obe <samuel.obe@outlook.com>
2021-08-19 22:06:22 -0400

    Added test text and image to detail view

--- Agno/View/DetailView.swift
+++ Agno/View/DetailView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct DetailView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var btnBack: some View {
+        Button(action: {self.presentationMode.wrappedValue.dismiss()}) {
+            Image(systemName: "chevron.left").padding()
+                .foregroundColor(.white).background(RoundedRectangle(cornerRadius: 12)
+                                                        .accentColor(.black)).opacity(0.7)
+            
+        }
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("BackgroundColor")
+            VStack {
+                
+                Image("MasterChief")
+                    .resizable().aspectRatio(contentMode: .fit)
+                HStack {
+                    Text("Full Name").foregroundColor(Color("SectionColor")).bold().font(.title2)
+                    Spacer()
+                }.padding(EdgeInsets(top: 20, leading: 20, bottom: 5, trailing: 0))
+                HStack {
+                    Text("Master Chief Petty Officer John-117").foregroundColor(Color(.white)).font(.title2)
+                    Spacer()
+                }.padding(.leading, 20)
+                Spacer()
+            }
+        }.ignoresSafeArea().navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: btnBack)
+        
     }
 }
 
