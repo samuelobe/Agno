0200969ccfc9a8ae9dbb557ad4706e336b4df619
Samuel Obe <samuel.obe@outlook.com>
2021-08-20 10:11:20 -0400

    Added a start screen

--- Agno/View/ContentView.swift
+++ Agno/View/ContentView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var action: Int? = 0
     var body: some View {
-        CameraView()
+        NavigationView {
+            ZStack {
+                Color.black.ignoresSafeArea()
+                VStack{
+                    Text("AGNO" ).foregroundColor(.white).font(Font.custom("CustomFont", size: 70))
+                    Text("AGNO is a celebrity-image recognition app which detects faces of celebrities with a single click..." ).foregroundColor(.white).font(Font.custom("CustomFont", size: 20)).multilineTextAlignment(.center)
+                    Spacer()
+                    NavigationLink(
+                        destination: CameraView(),
+                        tag: 1, selection: $action
+                    ) {
+                        Button(action: {self.action = 1}, label: {
+                            Text("Get Started").fontWeight(.bold)
+                                .font(.title)
+                                .padding(EdgeInsets(top: 20, leading: 100, bottom: 20, trailing: 100))
+                                .background(Color("ButtonColor"))
+                                .cornerRadius(40)
+                                .foregroundColor(.white)
+                        })
+                    }
+                    
+                }
+            }.navigationBarHidden(true)
+            .navigationBarTitle(Text("Home"))
+            
+            
+        }.preferredColorScheme(.dark)
+        
     }
 }
 
